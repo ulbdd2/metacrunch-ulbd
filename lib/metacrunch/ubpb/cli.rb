3d5ab77c9acabee8a4e116cd635c48876278726c
René Sprotte <rene.sprotte@provideal.net>
2015-05-26 17:19:36 +0200

    Add init_index and mab2snr commands.

--- lib/metacrunch/ubpb/cli.rb
+++ lib/metacrunch/ubpb/cli.rb
@@ -1,13 +1,16 @@
 require_relative "./commands/import_aleph_command"
-require_relative "./commands/init_mab_xml_storage_command"
+require_relative "./commands/init_index_command"
+require_relative "./commands/mab2snr_command"
 
 Metacrunch::Cli.setup("ubpb", "Commands for University Library Paderborn") do |r|
   #
-  # Init the MAB XML elasticsearch storage
+  # Init elasticsearch index for storage
   #
-  r.register(Metacrunch::UBPB::InitMabXmlStorageCommand) do |c|
-    c.usage "init_mab_xml_storage"
-    c.desc "Init ElasticSearch for MAB XML storage"
+  r.register(Metacrunch::UBPB::InitIndexCommand) do |c|
+    c.name  "init_index"
+    c.usage "init_index"
+    c.desc  "Init ElasticSearch for MAB XML storage"
+
     c.option :uri,
       desc: "ElasticSearch URI",
       type: :string,
@@ -26,18 +29,50 @@ Metacrunch::Cli.setup("ubpb", "Commands for University Library Paderborn") do |r
   # Load Aleph MAB XML into elastic search
   #
   r.register(Metacrunch::UBPB::ImportAlephCommand) do |c|
+    c.name  "import_aleph"
     c.usage "import_aleph FILES..."
-    c.desc "Import Aleph MAB XML files into ElasticSearch"
+    c.desc  "Import Aleph MAB XML files into ElasticSearch"
+
     c.option :uri,
-      desc: "ElasticSearch URI",
+      desc: "ElasticSearch URI for the (target) MAB XML data",
       type: :string,
-      required: true
+      default: "elasticsearch://localhost/mab_xml/document"
     c.option :log,
       desc: "Turn on ElasticSearch logging",
       type: :boolean,
       default: false
     c.option :bulk_size,
       desc: "Bulk size when writing to ElasticSearch",
+      type: :numeric,
+      default: 250
+    c.option :no_of_procs,
+      desc: "Number of parallel processes",
+      aliases: "-n",
+      type: :numeric,
+      default: 1
+  end
+
+  #
+  # Normalizes MAB to SNR
+  #
+  r.register(Metacrunch::UBPB::Mab2SnrCommand) do |c|
+    c.name  "mab2snr"
+    c.usage "mab2snr"
+    c.desc  "Normalize MAB into SNR"
+    c.option :source_uri,
+      desc: "ElasticSearch URI for the (source) MAB XML data",
+      type: :string,
+      default: "elasticsearch://localhost/mab_xml/document"
+    c.option :target_uri,
+      desc: "ElasticSearch URI for the (target) SNR data",
+      type: :string,
+      default: "elasticsearch://localhost/snr/document"
+    c.option :log,
+      desc: "Turn on ElasticSearch logging",
+      type: :boolean,
+      default: false
+    c.option :bulk_size,
+      desc: "Bulk size when reading/writing to ElasticSearch",
       type: :numeric,
       default: 250
     c.option :no_of_procs,
---
+++ lib/metacrunch/ubpb/commands/mab2snr_command.rb
@@ -0,0 +1,76 @@
+require "elasticsearch"
+require "ox"
+require "ruby-progressbar"
+
+module Metacrunch
+  module UBPB
+    class Mab2SnrCommand < Metacrunch::Command
+
+      def pre_perform
+        @source_uri  = options[:source_uri]
+        @target_uri  = options[:target_uri]
+        @log         = options[:log]
+        @bulk_size   = options[:bulk_size]
+        @no_of_procs = options[:no_of_procs]
+      end
+
+      def perform
+        source   = Metacrunch::Elasticsearch::Reader.new(@source_uri, query, log: @log)
+        count    = source.count
+        progress = ProgressBar.create(total: count.fdiv(@bulk_size).ceil)
+
+        shell.say "Processing #{count} records...", :green
+
+        bulk_enum = source.each.each_slice(@bulk_size)
+        Metacrunch::Parallel.new(bulk_enum) do |bulk|
+          target = Metacrunch::Elasticsearch::Writer.new(@target_uri, autoflush: false, bulk_size: @bulk_size, log: @log)
+
+          bulk.each do |hit|
+            id      = hit["_id"]
+            mab_xml = hit["_source"]["data"]
+
+            if mab_xml
+              mab = Metacrunch::Mab2::Document.from_aleph_mab_xml(mab_xml)
+              snr = Metacrunch::SNR.new
+
+              transformer = Transformer.new(source: mab, target: snr, options: {})
+              normalize(id, transformer)
+
+              target.write({id: id, data: Ox.dump(snr).force_encoding("utf-8")})
+            else
+              shell.say "Skipping empty mab with ID: #{id}", :red
+            end
+          end
+
+          target.flush
+        end.call(
+          in_processes: @no_of_procs,
+          on_process_finished: -> { progress.increment }
+        )
+      end
+
+    private
+
+      def query
+        {
+          query: {
+            match_all: {}
+          }
+        }
+      end
+
+      def normalize(id, transformer)
+        transformer.transform do
+          target.add("control", "id", id)
+        end
+
+        transformer.transform do
+          names  = source.datafields("100", ind1: "-").subfields("p").values
+          names += source.datafields("104", ind1: "a").subfields("p").values
+          target.add("search", "authors", names)
+        end
+      end
+
+    end
+  end
+end
