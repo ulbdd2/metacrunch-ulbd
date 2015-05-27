ef33987e212726a30f4f69052bc1f58a341fbdb4
René Sprotte <rene.sprotte@provideal.net>
2015-05-27 13:51:09 +0200

    Cleanup.

--- lib/metacrunch/ubpb/transformations/mab2snr/authors.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/authors.rb
@@ -2,15 +2,15 @@ module Metacrunch
   module UBPB
     module Transformations
       module MAB2SNR
-
         class Authors < Metacrunch::Transformer::Step
+
           def perform
             names  = source.datafields("100", ind1: "-").subfields("p").values
             names += source.datafields("104", ind1: "a").subfields("p").values
             target.add("search", "authors", names)
           end
-        end
 
+        end
       end
     end
   end
--- lib/metacrunch/ubpb/transformations/mab2snr/id.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/id.rb
@@ -2,13 +2,13 @@ module Metacrunch
   module UBPB
     module Transformations
       module MAB2SNR
-
         class Id < Metacrunch::Transformer::Step
+
           def perform
             target.add("control", "id", options[:source_id])
           end
-        end
 
+        end
       end
     end
   end
