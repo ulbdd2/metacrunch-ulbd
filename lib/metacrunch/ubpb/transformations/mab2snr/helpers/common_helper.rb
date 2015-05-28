a606cee0d9bff8937f620e2c14d09378019a0cc4
René Sprotte <rene.sprotte@provideal.net>
2015-05-28 13:12:03 +0200

    Add a helper module for common helpers.

---
+++ lib/metacrunch/ubpb/transformations/mab2snr/helpers/common_helper.rb
@@ -0,0 +1,13 @@
+module Metacrunch
+  module UBPB
+    module Transformations
+      module MAB2SNR
+        module Helpers
+          module CommonHelper
+
+          end
+        end
+      end
+    end
+  end
+end
