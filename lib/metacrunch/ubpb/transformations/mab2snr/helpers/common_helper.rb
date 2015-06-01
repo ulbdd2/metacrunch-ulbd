2111cd5f877ca8f64a85b7ac1d0e74990ccd286d
René Sprotte <rene.sprotte@provideal.net>
2015-06-01 11:37:18 +0200

    Add helper to determine if a record is a superorder.

--- lib/metacrunch/ubpb/transformations/mab2snr/helpers/common_helper.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/helpers/common_helper.rb
@@ -5,6 +5,23 @@ module Metacrunch
         module Helpers
           module CommonHelper
 
+            def is_superorder?
+              unless @is_superorder
+                f051 = source.controlfield("051") || []
+                f052 = source.controlfield("052") || []
+
+                @is_superorder = (
+                  f051.at(0) == "n" ||
+                  f051.at(0) == "t" ||
+                  f052.at(0) == "p" ||
+                  f052.at(0) == "r" ||
+                  f052.at(0) == "z"
+                )
+              end
+
+              @is_superorder
+            end
+
           end
         end
       end
