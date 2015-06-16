924fa7aca5ded20087549bf4c691ca1861ee1bab
René Sprotte <rene.sprotte@provideal.net>
2015-06-16 14:10:06 +0200

    Refactor.

--- lib/metacrunch/ubpb/transformations/mab2snr/date_of_publication.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/date_of_publication.rb
@@ -14,26 +14,12 @@ module Metacrunch
 
           def display_date
             unless @display_date
-              d = dates
-
-              @display_date = if d["f595"].present?
-                d["f595"]
+              @display_date = if dates["f595"].present?
+                dates["f595"]
               elsif helper.is_superorder?
-                if d["f425_b1"].present? and d["f425_c1"].present?
-                  if d["f425_b1"] == d["f425_c1"]
-                    d["f425_b1"]
-                  else
-                    "#{d['f425_b1']} – #{d['f425_c1']}"
-                  end
-                elsif d["f425_b1"].present? and not d["f425_c1"].present?
-                  "#{d['f425_b1']} –"
-                elsif d["f425_c1"].present? and not d["f425_b1"].present?
-                  "– #{d['f425_c1']}"
-                else
-                  d["f425_a1"].presence || d["f425_p1"].presence
-                end
+                superorder_date
               else
-                d["f425_a1"].presence || d["f425_p1"].presence
+                default_date
               end
             end
 
@@ -57,18 +43,38 @@ module Metacrunch
               @dates = {}
 
               # Erscheinungsjahr in Sortierform
-              @dates["f425_a1"] = source.datafields("425", ind1: "a", ind2: "1").subfields("a").first_value
+              @dates["f425_a"] = source.datafields("425", ind1: "a", ind2: "1").subfields("a").first_value
               # Erscheinungsjahr in Sortierform des frühsten Bandes
-              @dates["f425_b1"] = source.datafields("425", ind1: "b", ind2: "1").subfields("a").first_value
+              @dates["f425_b"] = source.datafields("425", ind1: "b", ind2: "1").subfields("a").first_value
               # Erscheinungsjahr in Sortierform des letzten Bandes
-              @dates["f425_c1"] = source.datafields("425", ind1: "c", ind2: "1").subfields("a").first_value
+              @dates["f425_c"] = source.datafields("425", ind1: "c", ind2: "1").subfields("a").first_value
               # Erscheinungsjahr in Sortierform eines Tonträgers
-              @dates["f425_p1"] = source.datafields("425", ind1: "p", ind2: "1").subfields("a").first_value
+              @dates["f425_p"] = source.datafields("425", ind1: "p", ind2: "1").subfields("a").first_value
               # Erscheinungsjahr der Quelle
               @dates["f595"]    = source.datafields("595").subfields("a").first_value # TODO: SUBFIELD A?
             end
 
             @dates
+          end
+
+          def default_date
+            dates["f425_a"].presence || dates["f425_p"].presence
+          end
+
+          def superorder_date
+            if dates["f425_b"].present? and dates["f425_c"].present?
+              if dates["f425_b"] == dates["f425_c"]
+                dates["f425_b"]
+              else
+                "#{dates['f425_b']} – #{dates['f425_c']}"
+              end
+            elsif dates["f425_b"].present? and not dates["f425_c"].present?
+              "#{dates['f425_b1']} –"
+            elsif dates["f425_c"].present? and not dates["f425_b"].present?
+              "– #{dates['f425_c']}"
+            else
+              default_date
+            end
           end
 
         end
