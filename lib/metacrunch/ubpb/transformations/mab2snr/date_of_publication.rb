48702357c4a7109bf3cabe802ea9165a7802a23b
René Sprotte <rene.sprotte@provideal.net>
2015-06-16 17:15:30 +0200

    Refactor.

--- lib/metacrunch/ubpb/transformations/mab2snr/date_of_publication.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/date_of_publication.rb
@@ -10,14 +10,13 @@ module Metacrunch
             target.add("sort",    "date_of_publication", search_date)
           end
 
-        private
-
           def display_date
             unless @display_date
-              @display_date = if dates["f595"].present?
-                dates["f595"]
+              @display_date = if dates["595"].present?
+                dates["595"]
               elsif helper.is_superorder?
-                superorder_date
+                range = superorder_date_range
+                [range[0], range[1]].join(" – ").strip
               else
                 default_date
               end
@@ -28,10 +27,12 @@ module Metacrunch
 
           def search_date
             unless @search_date
-              if display_date
-                date = display_date.dup
-                date.gsub!(/[^0-9]/i, "") # Entferne alle nicht numerischen Zeichen
-                @search_date = date
+              @search_date = if dates["595"].present?
+                dates["595"]
+              elsif helper.is_superorder?
+                superorder_date_range.find{|v| v.present?}
+              else
+                default_date
               end
             end
 
@@ -42,38 +43,30 @@ module Metacrunch
             unless @dates
               @dates = {}
 
-              # Erscheinungsjahr in Sortierform
-              @dates["f425_a"] = source.datafields("425", ind1: "a", ind2: "1").subfields("a").first_value
-              # Erscheinungsjahr in Sortierform des frühsten Bandes
-              @dates["f425_b"] = source.datafields("425", ind1: "b", ind2: "1").subfields("a").first_value
-              # Erscheinungsjahr in Sortierform des letzten Bandes
-              @dates["f425_c"] = source.datafields("425", ind1: "c", ind2: "1").subfields("a").first_value
-              # Erscheinungsjahr in Sortierform eines Tonträgers
-              @dates["f425_p"] = source.datafields("425", ind1: "p", ind2: "1").subfields("a").first_value
+              # Erscheinungsjahr in Ansetzungsform
+              @dates["425a"] = source.datafields("425", ind1: "a", ind2: "1").subfields("a").first_value
+              # Erscheinungsjahr in Ansetzungsform des frühsten Bandes
+              @dates["425b"] = source.datafields("425", ind1: "b", ind2: "1").subfields("a").first_value
+              # Erscheinungsjahr in Ansetzungsform des letzten Bandes
+              @dates["425c"] = source.datafields("425", ind1: "c", ind2: "1").subfields("a").first_value
+              # Publikationsdatum eines Tonträgers
+              @dates["425p"] = source.datafields("425", ind1: "p", ind2: "1").subfields("a").first_value
               # Erscheinungsjahr der Quelle
-              @dates["f595"]    = source.datafields("595").subfields("a").first_value # TODO: SUBFIELD A?
+              @dates["595"]   = source.datafields("595").subfields("a").first_value # TODO: SUBFIELD A?
             end
 
             @dates
           end
 
           def default_date
-            dates["f425_a"].presence || dates["f425_p"].presence
+            dates["425a"].presence || dates["425p"].presence
           end
 
-          def superorder_date
-            if dates["f425_b"].present? and dates["f425_c"].present?
-              if dates["f425_b"] == dates["f425_c"]
-                dates["f425_b"]
-              else
-                "#{dates['f425_b']} – #{dates['f425_c']}"
-              end
-            elsif dates["f425_b"].present? and not dates["f425_c"].present?
-              "#{dates['f425_b1']} –"
-            elsif dates["f425_c"].present? and not dates["f425_b"].present?
-              "– #{dates['f425_c']}"
+          def superorder_date_range
+            if dates["425b"].blank? and dates["425c"].blank?
+              [default_date]
             else
-              default_date
+              [dates["425b"], dates["425c"]].uniq
             end
           end
 
