55afa00cc624680b81f47a2d1d736b9f594cd891
René Sprotte <rene.sprotte@provideal.net>
2015-06-17 13:10:54 +0200

    Refactor.

--- lib/metacrunch/ubpb/transformations/mab2snr/edition.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/edition.rb
@@ -13,15 +13,13 @@ module Metacrunch
         private
 
           def edition
-            unless @edition
+            @edition ||= begin
               f403_1 = source.datafields("403", ind2: "1").subfields("a").first_value
               f403_2 = source.datafields("403", ind2: "2").subfields("a").first_value
               f407_1 = source.datafields("407", ind2: "1").subfields("a").first_value
 
-              @edition = f403_1.presence || f403_2.presence || f407_1.presence
+              f403_1.presence || f403_2.presence || f407_1.presence
             end
-
-            @edition
           end
 
         end
