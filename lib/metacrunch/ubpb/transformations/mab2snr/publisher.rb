f8417926032ec1f9d8100e31a44177089f76a7c9
René Sprotte <rene.sprotte@provideal.net>
2015-06-17 15:48:51 +0200

    Refactor.

--- lib/metacrunch/ubpb/transformations/mab2snr/publisher.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/publisher.rb
@@ -14,7 +14,7 @@ module Metacrunch
         private
 
           def publishers
-            unless @publishers
+            @publishers ||= begin
               f412_1 = source.datafields("412", ind2: "1").subfields("a").first_value
               f412_2 = source.datafields("412", ind2: "2").subfields("a").first_value
               f417_1 = source.datafields("417", ind2: "1").subfields("a").first_value
@@ -24,16 +24,16 @@ module Metacrunch
               f417 = f417_1.presence || f417_2.presence
               # ... weitere Verleger in 418 ohne Ortsangabe ignorieren wir
 
-              @publishers = []
-              @publishers << f412 if f412.present?
-              @publishers << f417 if f417.present?
-            end
+              publishers = []
+              publishers << f412 if f412.present?
+              publishers << f417 if f417.present?
 
-            @publishers
+              publishers
+            end
           end
 
           def places_of_publication
-            unless @places
+            @places ||= begin
               f410_1 = source.datafields("410", ind2: "1").subfields("a").first_value
               f410_2 = source.datafields("410", ind2: "2").subfields("a").first_value
               f415_1 = source.datafields("415", ind2: "1").subfields("a").first_value
@@ -42,12 +42,12 @@ module Metacrunch
               f410 = f410_1.presence || f410_2.presence
               f415 = f415_1.presence || f415_2.presence
 
-              @places = []
-              @places << f410 if f410.present?
-              @places << f415 if f415.present?
-            end
+              places = []
+              places << f410 if f410.present?
+              places << f415 if f415.present?
 
-            @places
+              places
+            end
           end
 
         end
