45b42d3f01ef2f91b1828b9ba4909a3af63ec131
René Sprotte <rene.sprotte@provideal.net>
2015-06-24 15:45:45 +0200

    Rewrote title field.

--- lib/metacrunch/ubpb/transformations/mab2snr/title.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/title.rb
@@ -26,16 +26,20 @@ module Metacrunch
           end
 
           def title_display
-            title = title_display_and_sort
-            title.delete!("<<")
-            title.delete!(">>")
-            title
+            if title_display_and_sort
+              title = title_display_and_sort.dup
+              title.delete!("<<")
+              title.delete!(">>")
+              title
+            end
           end
 
           def title_sort
-            title = title_display_and_sort
-            title.gsub!(/<<.*>>\s/, "")
-            title
+            if title_display_and_sort
+              title = title_display_and_sort.dup
+              title.gsub!(/<<.*>>\s/, "")
+              title
+            end
           end
 
           #
@@ -44,73 +48,63 @@ module Metacrunch
           #   f089_1.gsub(/.*(bd|Bd).*\,/, '') # Try to remove volume count from
           #
           def title_display_and_sort
-            p = title_parts
+            @title_display_and_sort ||= begin
+              p    = title_parts
+              ind2 = helper.is_superorder? ? "2" : "1"
 
-            if p["310_1"]
-              p["310_1"]
-            elsif p["331_1"] && p["334_1"] && p["335_1"]
-              "#{p['331_1']} [#{p['334_1']}] : #{p['335_1']}"
-            elsif p["331_1"] && p["335_1"]
-              "#{p['331_1']} : #{p['335_1']}"
-            elsif p["331_1"] && p["334_1"]
-              "#{p['331_1']} [#{p['334_1']}]"
-            elsif p["331_1"]
-              p["331_1"]
-            elsif p["331_2"]
-              p["331_2"]
-            elsif p["089_1"]
-              p["089_1"]
-            else
-              "n.a."
+              title = if p["360_#{ind2}"].present?
+                p["310_#{ind2}"].first
+              elsif p["331_1"].present?
+                p["331_1"].first
+              elsif p["331_2"].present?
+                p["331_2"].first
+              elsif p["089_1"].present?
+                p["089_1"].first
+              else
+                ""
+              end
+
+              if p["334_1"].present?
+                title += " [#{p['334_1'].first}]"
+              elsif p["334_2"].present?
+                title += " [#{p['334_2'].first}]"
+              end
+
+              if p["331_1"].present? && p["335_1"].present?
+                title += " : #{p['335_1'].first}"
+              end
+
+              if p["331_1"].present? && p["361_1"].present?
+                title += ". #{p['361_1'].first}"
+              end
+
+              title
             end
           end
 
           def title_parts
-            unless @title_parts
+            @title_parts ||= begin
               parts = {}
 
-              parts["331_2"] = source.datafields("331", ind2: "2").subfields("a").first_value
-              parts["333_2"] = source.datafields("333", ind2: "2").subfields("a").first_value
-              parts["335_2"] = source.datafields("335", ind2: "2").subfields("a").first_value
-              parts["360_2"] = source.datafields("360", ind2: "2").subfields("a").values
+              fields.each do |field|
+                field_s = sprintf "%03d", field
 
-              parts["089_1"] = source.datafields("089", ind2: "1").subfields('a').first_value
-              parts["304_1"] = source.datafields("304", ind2: "1").subfields('a').first_value
-              parts["310_1"] = source.datafields("310", ind2: "1").subfields('a').first_value
-              parts["331_1"] = source.datafields("331", ind2: "1").subfields('a').first_value
-              parts["333_1"] = source.datafields("333", ind2: "1").subfields('a').first_value
-              parts["335_1"] = source.datafields("335", ind2: "1").subfields('a').first_value
-              parts["340_1"] = source.datafields("340", ind2: "1").subfields('a').first_value
-              parts["341_1"] = source.datafields("341", ind2: "1").subfields('a').first_value
-              parts["360_1"] = source.datafields("360", ind2: "1").subfields('a').values
-
-              (342..355).each do |f|
-                parts["#{f}_1"] = source.datafields("#{f}", ind2: "1").subfields('a').first_value
+                parts["#{field_s}_1"] = values_for(field_s, 1)
+                parts["#{field_s}_2"] = values_for(field_s, 2)
               end
 
-              parts["370_1"] = source.datafields("370", ind2: "1").subfields('a').values
-              parts["376_1"] = source.datafields("376", ind2: "1").subfields('a').values
-
-              (451..491).step(10).each do |f|
-                parts["#{f}_1"] = source.datafields("#{f}", ind2: "1").subfields('a').first_value
-              end
-
-              parts["502_1"] = source.datafields("502", ind2: "1").subfields('a').first_value
-              parts["504_1"] = source.datafields("504", ind2: "1").subfields('a').first_value
-              parts["505_1"] = source.datafields("505", ind2: "1").subfields('a').first_value
-
-              (526..534).each do |f|
-                parts["#{f}_1"] = source.datafields("#{f}", ind2: "1").subfields('a').first_value
-              end
-
-              parts["621_1"] = source.datafields("621", ind2: "1").subfields('a').first_value
-              parts["627_1"] = source.datafields("627", ind2: "1").subfields('a').first_value
-              parts["633_1"] = source.datafields("633", ind2: "1").subfields('a').first_value
-
-              @title_parts = parts
+              parts
             end
+          end
 
-            @title_parts
+          def values_for(field, ind2)
+            source.datafields("#{field}", ind2: "#{ind2}").subfields("a").values
+          end
+
+          def fields
+            fields = 89, 304, 310, 331, 333, 334, 335, *(340..355), 360, 361,
+              365, 370, 376, *(451..491).step(10).to_a, 502, 504, 505, 517,
+              *(526..534), 590, 592, 597, 621, 624, 627, 630, 633
           end
 
         end
