d39007718d40b0de1dd6770db449323cdd3f0db0
René Sprotte <rene.sprotte@provideal.net>
2015-06-24 16:14:20 +0200

    Use mab2snr test helper.

--- spec/mab2snr/creation_date_spec.rb
+++ spec/mab2snr/creation_date_spec.rb
@@ -1,30 +1,13 @@
 describe Metacrunch::UBPB::Transformations::MAB2SNR::CreationDate do
-  let(:transformer) do
-    transform(
-      Metacrunch::UBPB::Transformations::MAB2SNR::CreationDate,
-      mab,
-      Metacrunch::SNR.new
-    )
-  end
 
   it "creation_date should be 20070905" do
-    date = transformer.target.values("control/creation_date").first
-    expect(date).to eq("20070905")
-  end
+    mab = mab_builder do
+      datafield("LOC", ind1: " ", ind2: " ") { subfield("k", "20070905") }
+      datafield("LOC", ind1: " ", ind2: " ") { subfield("k", "20070906") }
+    end
 
-private
-
-  def mab
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <datafield tag="LOC" ind1=" " ind2=" ">
-          <subfield code="k">20070905</subfield>
-        </datafield>
-        <datafield tag="LOC" ind1=" " ind2=" ">
-          <subfield code="k">20070906</subfield>
-        </datafield>
-      </record>
-    XML
+    result = mab2snr(mab)
+    expect(result.first_value("control/creation_date")).to eq("20070905")
   end
 
 end
