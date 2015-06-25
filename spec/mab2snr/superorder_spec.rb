685a1b7ed7d920f926332339136970cd122e40e1
René Sprotte <rene.sprotte@provideal.net>
2015-06-25 15:05:54 +0200

    Refactor.

--- spec/mab2snr/superorder_spec.rb
+++ spec/mab2snr/superorder_spec.rb
@@ -1,64 +1,41 @@
 describe Metacrunch::UBPB::Transformations::MAB2SNR::Superorder do
 
-  it "is superorder if 051(0) is 'n'" do
-    result = perform(test_field("051", "n"))
-    expect(result[:superorder]).to be(true)
+  it "Default is false" do
+    result = mab2snr(mab_builder) # empty mab
+    expect(result.first_value("control/superorder")).to be(false)
   end
 
-  it "is superorder if 051(0) is 't'" do
-    result = perform(test_field("051", "t"))
-    expect(result[:superorder]).to be(true)
+  it "Is superorder if 051(0) is 'n'" do
+    result = mab2snr(test_mab("051", "n"))
+    expect(result.first_value("control/superorder")).to be(true)
   end
 
-  it "is no superorder if 051(0) is not 'n' or 't'" do
-    result = perform(test_field("051", "x"))
-    expect(result[:superorder]).to be(false)
+  it "Is superorder if 051(0) is 't'" do
+    result = mab2snr(test_mab("051", "t"))
+    expect(result.first_value("control/superorder")).to be(true)
   end
 
-  it "is superorder if 052(0) is 'p'" do
-    result = perform(test_field("052", "p"))
-    expect(result[:superorder]).to be(true)
+  it "Is superorder if 052(0) is 'p'" do
+    result = mab2snr(test_mab("052", "p"))
+    expect(result.first_value("control/superorder")).to be(true)
   end
 
-  it "is superorder if 052(0) is 'r'" do
-    result = perform(test_field("052", "r"))
-    expect(result[:superorder]).to be(true)
+  it "Is superorder if 052(0) is 'r'" do
+    result = mab2snr(test_mab("052", "r"))
+    expect(result.first_value("control/superorder")).to be(true)
   end
 
-  it "is superorder if 052(0) is 'z'" do
-    result = perform(test_field("052", "z"))
-    expect(result[:superorder]).to be(true)
+  it "Is superorder if 052(0) is 'z'" do
+    result = mab2snr(test_mab("052", "z"))
+    expect(result.first_value("control/superorder")).to be(true)
   end
-
-  it "is no superorder if 052(0) is 'p' or 'r' or 'z'" do
-    result = perform(test_field("052", "x"))
-    expect(result[:superorder]).to be(false)
-  end
-
 
 private
 
-  def perform(source)
-    transformer = transform(
-      Metacrunch::UBPB::Transformations::MAB2SNR::Superorder,
-      source,
-      Metacrunch::SNR.new
-    )
-
-    superorder = transformer.target.values("control/superorder").first
-
-    {
-      transformer: transformer,
-      superorder: superorder
-    }
-  end
-
-  def test_field(field, value)
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <controlfield tag="#{field}">#{value}||||</controlfield>
-      </record>
-    XML
+  def test_mab(field, value)
+    mab_builder do
+      controlfield(field, "#{value}||||")
+    end
   end
 
 end
