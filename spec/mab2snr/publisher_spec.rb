09d4bd738c0ea0d25b197d3360274998cb9b111e
René Sprotte <rene.sprotte@provideal.net>
2015-06-25 14:35:14 +0200

    Refactor.

--- spec/mab2snr/publisher_spec.rb
+++ spec/mab2snr/publisher_spec.rb
@@ -1,107 +1,56 @@
 describe Metacrunch::UBPB::Transformations::MAB2SNR::Publisher do
 
-  it "412_1 works" do
-    result = perform(test_412_1)
-    expect(result[:display].first).to eq("412_PUBLISHER_1")
-    expect(result[:search].first).to eq("412_PUBLISHER_1")
+  it "412 ind2=1 works" do
+    mab = mab_builder do
+      datafield("412", ind1: "-", ind2: "1") { subfield("a", "SOME PUBLISHER") }
+    end
+
+    result = mab2snr(mab)
+    expect(result.first_value("display/publisher")).to eq("SOME PUBLISHER")
+    expect(result.first_value("search/publisher")).to eq("SOME PUBLISHER")
   end
 
-  it "412_2 works" do
-    result = perform(test_412_2)
-    expect(result[:display].first).to eq("412_PUBLISHER_2")
-    expect(result[:search].first).to eq("412_PUBLISHER_2")
+  it "412 ind2=2 works" do
+    mab = mab_builder do
+      datafield("412", ind1: "-", ind2: "2") { subfield("a", "SOME PUBLISHER") }
+    end
+
+    result = mab2snr(mab)
+    expect(result.first_value("display/publisher")).to eq("SOME PUBLISHER")
+    expect(result.first_value("search/publisher")).to eq("SOME PUBLISHER")
   end
 
-  it "417_1 works" do
-    result = perform(test_417_1)
-    expect(result[:display].first).to eq("417_PUBLISHER_1")
-    expect(result[:search].first).to eq("417_PUBLISHER_1")
+  it "417 ind2=1 works" do
+    mab = mab_builder do
+      datafield("417", ind1: "-", ind2: "1") { subfield("a", "SOME PUBLISHER") }
+    end
+
+    result = mab2snr(mab)
+    expect(result.first_value("display/publisher")).to eq("SOME PUBLISHER")
+    expect(result.first_value("search/publisher")).to eq("SOME PUBLISHER")
   end
 
-  it "417_2 works" do
-    result = perform(test_417_2)
-    expect(result[:display].first).to eq("417_PUBLISHER_2")
-    expect(result[:search].first).to eq("417_PUBLISHER_2")
+  it "417 ind2=2 works" do
+    mab = mab_builder do
+      datafield("417", ind1: "-", ind2: "2") { subfield("a", "SOME PUBLISHER") }
+    end
+
+    result = mab2snr(mab)
+    expect(result.first_value("display/publisher")).to eq("SOME PUBLISHER")
+    expect(result.first_value("search/publisher")).to eq("SOME PUBLISHER")
   end
 
   it "412_1 and 417_1 works" do
-    result = perform(test_412_1_and_417_1)
-    expect(result[:display][0]).to eq("412_PUBLISHER_1")
-    expect(result[:search][0]).to eq("412_PUBLISHER_1")
-    expect(result[:display][1]).to eq("417_PUBLISHER_1")
-    expect(result[:search][1]).to eq("417_PUBLISHER_1")
-  end
+    mab = mab_builder do
+      datafield("412", ind1: "-", ind2: "1") { subfield("a", "SOME PUBLISHER") }
+      datafield("417", ind1: "-", ind2: "1") { subfield("a", "ANOTHER PUBLISHER") }
+    end
 
-private
-
-  def perform(source)
-    transformer = transform(
-      Metacrunch::UBPB::Transformations::MAB2SNR::Publisher,
-      source,
-      Metacrunch::SNR.new
-    )
-
-    display = transformer.target.values("display/publisher")
-    search  = transformer.target.values("search/publisher")
-
-    {
-      transformer: transformer,
-      display: display,
-      search: search
-    }
-  end
-
-  def test_412_1
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <datafield tag="412" ind1="-" ind2="1">
-          <subfield code="a">412_PUBLISHER_1</subfield>
-        </datafield>
-      </record>
-    XML
-  end
-
-  def test_412_2
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <datafield tag="412" ind1="-" ind2="2">
-          <subfield code="a">412_PUBLISHER_2</subfield>
-        </datafield>
-      </record>
-    XML
-  end
-
-  def test_417_1
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <datafield tag="417" ind1="-" ind2="1">
-          <subfield code="a">417_PUBLISHER_1</subfield>
-        </datafield>
-      </record>
-    XML
-  end
-
-  def test_417_2
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <datafield tag="417" ind1="-" ind2="2">
-          <subfield code="a">417_PUBLISHER_2</subfield>
-        </datafield>
-      </record>
-    XML
-  end
-
-  def test_412_1_and_417_1
-    Metacrunch::Mab2::Document.from_aleph_mab_xml <<-XML
-      <record>
-        <datafield tag="412" ind1="-" ind2="1">
-          <subfield code="a">412_PUBLISHER_1</subfield>
-        </datafield>
-        <datafield tag="417" ind1="-" ind2="1">
-          <subfield code="a">417_PUBLISHER_1</subfield>
-        </datafield>
-      </record>
-    XML
+    result = mab2snr(mab)
+    expect(result.values("display/publisher")[0]).to eq("SOME PUBLISHER")
+    expect(result.values("display/publisher")[1]).to eq("ANOTHER PUBLISHER")
+    expect(result.values("search/publisher")[0]).to eq("SOME PUBLISHER")
+    expect(result.values("search/publisher")[1]).to eq("ANOTHER PUBLISHER")
   end
 
 end
