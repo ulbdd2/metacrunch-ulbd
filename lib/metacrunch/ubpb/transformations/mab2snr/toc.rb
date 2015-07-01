4f489a7a0d7a6147d6d78346d60e2b6e3530261f
René Sprotte <rene.sprotte@provideal.net>
2015-07-01 16:18:11 +0200

    Handle nil case.

--- lib/metacrunch/ubpb/transformations/mab2snr/toc.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/toc.rb
@@ -5,15 +5,17 @@ module Metacrunch
         class Toc < Metacrunch::Transformer::Step
 
           def perform
-            target.add("search", "toc", toc)
+            target.add("search", "toc", toc) unless toc.blank?
           end
 
         private
 
           def toc
-            source.datafields("TXT").map do |datafield|
-              datafield.subfields("a").first_value
-            end.join(" ")
+            @toc ||= begin
+              source.datafields("TXT").map do |datafield|
+                datafield.subfields("a").first_value
+              end.join(" ")
+            end
           end
 
         end
--- lib/metacrunch/ubpb/transformations/mab2snr/zdb_id.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/zdb_id.rb
@@ -5,13 +5,15 @@ module Metacrunch
         class ZDBId < Metacrunch::Transformer::Step
 
           def perform
-            target.add("control", "zdb_id", zdb_id)
+            target.add("control", "zdb_id", zdb_id) unless zdb_id.blank?
           end
 
         private
 
           def zdb_id
-            source.datafields("025", ind1: "z").subfields("a").first_value
+            @zdb_id ||= begin
+              source.datafields("025", ind1: "z").subfields("a").first_value
+            end
           end
 
         end
