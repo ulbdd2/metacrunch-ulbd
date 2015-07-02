3786f36ae40e4de7d14425eb26154abfa8d78348
René Sprotte <rene.sprotte@provideal.net>
2015-07-02 16:59:45 +0200

    Refactor.

--- lib/metacrunch/ubpb/transformations/mab2snr/volume_count.rb
+++ lib/metacrunch/ubpb/transformations/mab2snr/volume_count.rb
@@ -5,20 +5,67 @@ module Metacrunch
         class VolumeCount < Metacrunch::Transformer::Step
 
           def perform
-            target.add("control", "volume_count", volume_count)
+            target.add("display", "volume_count", display)
+            target.add("sort", "volume_count", sort)
           end
 
         private
 
-          # TODO: Noch unfertig. Vergleich mabmapper Implementierung.
-          def volume_count
-            volumes = []
+          #
+          # Bandangaben in Vorlageform
+          #
+          def display
+            counts = []
 
-            (456..496).step(10) do |f|
-              volumes += source.datafields("#{f}", ind2: "1").subfields("a").values
+            counts << source.datafields("089").subfields("a").first_value
+            (455..495).step(10) do |f|
+              counts << source.datafields("#{f}", ind2: "1").subfields("a").first_value
             end
 
-            volumes.find{ |v| v.present? }.to_i
+            # Take the first that holds a value
+            counts.find{ |v| v.present? }
+          end
+
+          #
+          # Bandangaben in Sortierform
+          #
+          # Die Werte können in der einfachsten Form Zahlen sein.
+          # Es kann aber auch vorkommen, dass Gruppen gebildet werden. Diese
+          # werden durch ein Komma getrennt. Zusätzlich ist eine
+          # lexikografische Sortierform denkbar.
+          #
+          # Beispiele: "10", "42", "5,4", "5,4,2", "m", "m,mex"
+          #
+          def sort
+            counts = []
+
+            counts << source.datafields("090", ind2: "1").subfields("a").first_value
+            (456..496).step(10) do |f|
+              counts << source.datafields("#{f}", ind2: "1").subfields("a").first_value
+            end
+
+            # Take the first that holds a value
+            count = counts.find{ |v| v.present? }
+
+            # Make sortable "value"
+            build_sortable_string(count) unless count.blank?
+          end
+
+          def build_sortable_string(count)
+            # extract the count groups
+            count_groups = count.split(',')
+            # make sure we have always 4 groups
+            count_groups = Array.new(4) { |i| count_groups[i] }
+            # make sure every group is a string
+            count_groups.map!{ |g| g.to_s }
+            # clean things up a bit
+            count_groups.map!{ |g| g.gsub(/\[|\]/, '').strip }
+            # fill up each group to 6 characters
+            count_groups.map!{ |g| g.rjust(6, '0') }
+            # make sure each group is really 6 characters.
+            count_groups.map!{ |g| g[0..5] }
+            # finally join
+            count_groups.join
           end
 
         end
