86ea1d6b45d262bb0d733546f73ab6e4f2c3e3cf
Michael Sievers <michael_sievers@web.de>
2015-08-21 16:00:59 +0200

    Added superorder

--- etc/mabmapper-old-todo.rb
+++ etc/mabmapper-old-todo.rb
@@ -256,66 +256,6 @@ module Mabmapper
     end
 
     #
-    # JSON encoded field to store superorder related informations
-    #
-    field :superorder_display do
-      superorders = []
-
-      # Link zur Überordung eines mehrbändigen Werkes
-      superorders << {
-        ht_number: doc.field('010', ind2: '1').subfield('a').get.value,
-        label: doc.field('331', ind2: '2').subfield('a').get.value,
-        volume_count: doc.field('089', ind2: '1').subfield('a').get.value # Bandzählung dieses Werkes innerhalb der entsprechenden Überordnung
-      }
-
-      # 451 ff
-      (451..491).step(10) do |f|
-        superorders << {
-          ht_number: doc.field("#{f+2}", ind2: '1').subfield('a').get.value,
-          label: [doc.field("#{f}", ind2: '1').subfield('a').get.value, doc.field("#{f}", ind2: '2').subfield('a').get.value].compact.reject { |label| label[/\A\.\.\.\s+(;|:)/] }.first,
-          volume_count: doc.field("#{f+4}", ind2: '1').subfield('a').get.value
-        }
-      end
-
-      superorders
-      .map(&:presence)
-      .delete_if { |element| element[:label].blank? }
-      .each do |element|
-        # remove 'not sort' indicators from label
-        element[:label].try(:gsub!, /<<|>>/, '')
-
-        # remove leading '... ' from label
-        element[:label].try(:gsub!, /\A\.\.\.\s+/, '')
-
-        # get label additions (everything behind the first ':') and make it a clean array
-        element[:label_additions] = if element[:label].present?
-          element[:label][/:.*/]
-          .try(:gsub, /(\A:)|(,\Z)/, '')
-          .try(:strip)
-          .try(:gsub, /,/, ';')
-          .try(:split, ';')
-          .try(:map, &:strip)
-        end
-
-        # remove every label addition that is also in volume count (space removement and downcasing are done for more fuzzy matching e.g. between 'Faz. 4' and 'faz.4'
-        if element[:label_additions].present? && element[:volume_count].present?
-          volume_count_elements = element[:volume_count].gsub(/,|:|;/, ';').split(';').map { |e| e.gsub(/\s+/, '').downcase } # volume_count elements without spaces for more fuzzy comparing
-
-          element[:label_additions].reject! { |label_addition| volume_count_elements.include? label_addition.gsub(/\s+/, '').downcase }
-          element[:label_additions].reject! { |label_addition| ['bd', 'band'].any? { |forbidden_label_addition| label_addition.downcase.starts_with? forbidden_label_addition } } # additional remove every Bd. or Band
-          element[:label_additions] = element[:label_additions].presence
-        end
-
-        # remove any label additions from the label
-        element[:label].try(:gsub!, /:.*\Z/, '')
-        element[:label].try(:gsub!, /;.*\Z/, '')
-        element[:label].try(:strip!)
-
-        element[:volume_count].try(:gsub!, /<.*>/, '')
-      end.uniq.map(&:to_json)
-    end
-
-    #
     # Linked superorders (only ht_numbers) + ht_number if primary form (if any)
     #
     # @depends_on: superorder_display
