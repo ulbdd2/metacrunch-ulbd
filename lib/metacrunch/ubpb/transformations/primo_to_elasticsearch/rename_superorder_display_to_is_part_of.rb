b514498197532d67c79df6afbebfac5a823c2bd6
Michael Sievers <michael_sievers@web.de>
2015-10-13 12:13:14 +0200

    WIP

--- lib/metacrunch/ubpb/transformations/primo_to_elasticsearch/rename_superorder_display_to_is_part_of.rb
+++ lib/metacrunch/ubpb/transformations/primo_to_elasticsearch/rename_superorder_display_to_is_part_of.rb
@@ -4,6 +4,8 @@ require_relative "../primo_to_elasticsearch"
 
 class Metacrunch::UBPB::Transformations::PrimoToElasticsearch::RenameSuperorderDisplayToIsPartOf < Metacrunch::Transformator::Transformation::Step
   def call
+    target.delete("is_part_of") # has to be deleted any way, because there might be old data
+
     if superorder_display = target.delete("superorder_display")
       Metacrunch::Hash.add(target, "is_part_of", superorder_display)
     end
