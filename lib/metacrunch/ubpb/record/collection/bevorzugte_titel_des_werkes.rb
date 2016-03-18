1db1a76323ef402d9a39d641df9fa4b18cd9468d
Michael Sievers <michael_sievers@web.de>
2016-03-18 11:20:31 +0100

    WIP

--- lib/metacrunch/ubpb/record/collection/bevorzugte_titel_des_werkes.rb
+++ lib/metacrunch/ubpb/record/collection/bevorzugte_titel_des_werkes.rb
@@ -2,14 +2,14 @@ require_relative "../collection"
 require_relative "../element/titel"
 
 class Metacrunch::UBPB::Record::Collection::BevorzugteTitelDesWerkes < Metacrunch::UBPB::Record::Collection
-  def initialize(document, tags, element_class, options = {})
-    options[:include] = [options[:include]].compact.flatten(1)
+  def initialize(datafields, options = {})
+    include_options_values = [options[:include]].compact.flatten(1)
     ind1 = ["-"]
 
-    if options[:include].include?("in der Manifestation verkörperte Werke") || options[:include].include?("in Beziehung stehende Werke")
+    if include_options_values.include?("in der Manifestation verkörperte Werke") || include_options_values.include?("in Beziehung stehende Werke")
       ind1 << "t"
     end
 
-    super(document, tags, element_class, options.merge(ind1: ind1))
+    super(datafields, options.merge(ind1: ind1))
   end
 end
