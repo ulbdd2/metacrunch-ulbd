7d08599ab777f02718b1c944614a9664226db252
René Sprotte <rene.sprotte@provideal.net>
2016-04-22 10:26:45 +0200

    Remove CLI stuff.

--- lib/metacrunch/ubpb.rb
+++ lib/metacrunch/ubpb.rb
@@ -1,11 +1,9 @@
 require "metacrunch"
 require "metacrunch/mab2"
-require "metacrunch/elasticsearch"
 require_relative "./ubpb/version"
 
 module Metacrunch
   module UBPB
-    require_relative "./ubpb/cli"
     require_relative "./ubpb/transformations"
 
     def self.root
