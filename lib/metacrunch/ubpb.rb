083c748253f045d2ef35c10cf04880cbdfd9f415
René Sprotte <rene.sprotte@provideal.net>
2016-04-22 10:52:21 +0200

    Notes about metacrunch/transformator and metacrunch/hash.

--- lib/metacrunch/ubpb.rb
+++ lib/metacrunch/ubpb.rb
@@ -4,8 +4,8 @@ require_relative "./ubpb/version"
 
 module Metacrunch
   module UBPB
-    require_relative "./transformator"
-    require_relative "./hash"
+    require_relative "./transformator" # FIXME: Should be moved to the Metacrunch::UBPB namespace
+    require_relative "./hash" # FIXME: Should be moved to the Metacrunch::UBPB namespace
     require_relative "./ubpb/transformations"
 
     def self.root
