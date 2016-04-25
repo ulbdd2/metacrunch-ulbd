1d1c5ce6f423fd70388e82cf07e1e995b783b040
Michael Sievers <michael_sievers@web.de>
2016-04-25 11:33:42 +0200

    Added tests for add_resource_link and add_link_to_toc

--- spec/metacrunch/ubpb/transformations/mab_to_primo/add_links_spec.rb
+++
@@ -1,3 +0,0 @@
-describe Metacrunch::UBPB::Transformations::MabToPrimo::AddLinks do
-  define_field_test "001700003", links: []
-end
---
+++ spec/metacrunch/ubpb/transformations/mab_to_primo/add_resource_link_spec.rb
@@ -0,0 +1,3 @@
+describe Metacrunch::UBPB::Transformations::MabToPrimo::AddResourceLink do
+  define_field_test "000858612", resource_link: "http://www.die-bonn.de/id/457"
+end
