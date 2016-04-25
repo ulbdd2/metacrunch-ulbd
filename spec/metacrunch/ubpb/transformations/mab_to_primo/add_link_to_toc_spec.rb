aa9662674700d9fc7dff0a45e5590b04414d3078
Michael Sievers <michael_sievers@web.de>
2016-04-25 11:33:42 +0200

    Added tests for add_resource_link and add_link_to_toc

---
+++ spec/metacrunch/ubpb/transformations/mab_to_primo/add_link_to_toc_spec.rb
@@ -0,0 +1,3 @@
+describe Metacrunch::UBPB::Transformations::MabToPrimo::AddLinkToToc do
+  define_field_test "001700003", link_to_toc: "http://digitool.hbz-nrw.de:1801/webclient/DeliveryManager?pid=5153162&custom_att_2=simple_viewer"
+end
