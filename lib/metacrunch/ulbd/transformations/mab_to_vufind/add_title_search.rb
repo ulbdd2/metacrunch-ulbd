f5aee280c9fca7acc3b5e4138dc4d81e238892dc
rby <rby@pc1205.AD.hhu.de>
2016-09-06 14:24:56 +0200

    Titel in Originalschrift zur Suche hinzugefügt

--- lib/metacrunch/ulbd/transformations/mab_to_vufind/add_title_search.rb
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_title_search.rb
@@ -2,6 +2,7 @@ require "metacrunch/hash"
 require "metacrunch/transformator/transformation/step"
 require_relative "../mab_to_vufind"
 require_relative "./add_title"
+require_relative "./add_title_os"
 
 class Metacrunch::ULBD::Transformations::MabToVufind::AddTitleSearch < Metacrunch::Transformator::Transformation::Step
   def call
@@ -13,10 +14,11 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddTitleSearch < Metacrunc
   def title_search
     search_titles = []
     search_titles << title_display
+    search_titles << title_display_os
 
     search_titles << source.datafields('304', ind2: '1').subfields('a').values
-    
-    (342..355).each do |f|
+        
+    (340..355).each do |f|
       search_titles << source.datafields("#{f}", ind2: '1').subfields('a').values
     end
 
@@ -35,6 +37,8 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddTitleSearch < Metacrunc
     search_titles << source.datafields('621', ind2: '1').subfields('a').values
     search_titles << source.datafields('627', ind2: '1').subfields('a').values
     search_titles << source.datafields('633', ind2: '1').subfields('a').values
+    search_titles << source.datafields('670', ind2: '1').subfields('a').values
+    search_titles << source.datafields('675', ind2: '1').subfields('a').values
 
     # frühere Titel (RDA)
     source.datafields('375', ind2: [:blank, '1']).each do |_datafield|
@@ -65,6 +69,41 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddTitleSearch < Metacrunc
     source.get("EST der Nebeneintragungen").each do |element|
       search_titles << element.get("Titel")
     end
+  
+    
+    search_titles << source.datafields('C04', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C40', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C41', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C43', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C44', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C45', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C47', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C48', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C49', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C51', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C52', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C53', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C55', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C70', ind2: '1').subfields('a').values
+    search_titles << source.datafields('C76', ind2: '1').subfields('a').values
+    search_titles << source.datafields('D51', ind2: '1').subfields('a').values
+    search_titles << source.datafields('D61', ind2: '1').subfields('a').values
+    search_titles << source.datafields('D71', ind2: '1').subfields('a').values
+    search_titles << source.datafields('D81', ind2: '1').subfields('a').values
+    search_titles << source.datafields('D91', ind2: '1').subfields('a').values
+    search_titles << source.datafields('E02', ind2: '1').subfields('a').values
+    search_titles << source.datafields('E04', ind2: '1').subfields('a').values
+    search_titles << source.datafields('E05', ind2: '1').subfields('a').values
+    search_titles << source.datafields('E27', ind2: '1').subfields('a').values
+    search_titles << source.datafields('E31', ind2: '1').subfields('a').values
+    search_titles << source.datafields('E33', ind2: '1').subfields('a').values
+    search_titles << source.datafields('F70', ind2: '1').subfields('a').values
+    search_titles << source.datafields('F75', ind2: '1').subfields('a').values
+    
+    source.datafields('C75', ind2: [:blank, '1']).each do |_datafield|
+      _früherer_titel = _datafield.subfields('a').value
+      search_titles << _früherer_titel
+    end  
     
     search_titles
     .flatten
@@ -86,10 +125,14 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddTitleSearch < Metacrunc
     .compact
     .uniq
   end
+  
 
   private
 
   def title_display
     target.try(:[], "title_display") || self.class.parent::AddTitleDisplay.new(source: source).call
+  end
+  def title_display_os
+    target.try(:[], "title_display_os") || self.class.parent::AddTitleOs.new(source: source).call
   end
 end
