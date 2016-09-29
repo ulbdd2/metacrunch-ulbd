bc16f5bfaf7b1df44168a0c6ddde5136ebe63cdd
rby <rby@pc1205.AD.hhu.de>
2016-09-29 14:58:25 +0200

    Feld für Verlinkungen parallele Ausgaben etc.

---
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_relation_link.rb
@@ -0,0 +1,37 @@
+require "metacrunch/hash"
+require "metacrunch/transformator/transformation/step"
+require_relative "../mab_to_vufind"
+
+class Metacrunch::ULBD::Transformations::MabToVufind::AddRelationLink < Metacrunch::Transformator::Transformation::Step
+  def call
+    target ? Metacrunch::Hash.add(target, "relation_link_txt_mv", relationlink) : relationlink
+  end
+
+  private
+
+  def relationlink
+    
+    relationlink = []
+
+    source.datafields('PLK').each do |datafield|
+           typ        = datafield.subfields('a').value
+           url        = datafield.subfields('b').value
+           label      = datafield.subfields('n').value
+        #fulltext_links << url       
+        #fulltext_links << label
+            
+      relationlink << {
+            typ: typ,
+            sys_no: url,
+            label: label
+         } 
+      end
+    
+    
+    relationlink.compact.presence.map(&:to_json)
+  end
+end
+
+
+
+
