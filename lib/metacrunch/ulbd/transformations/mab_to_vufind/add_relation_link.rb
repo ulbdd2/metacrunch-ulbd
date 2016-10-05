db9f559f97fc93cdb8066e67a79342a8748b967e
rby <rby@pc1205.AD.hhu.de>
2016-10-05 13:40:38 +0200

    funktioniert jetzt auch beim Windelfutsch (ohne PLK)

--- lib/metacrunch/ulbd/transformations/mab_to_vufind/add_relation_link.rb
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_relation_link.rb
@@ -15,20 +15,20 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddRelationLink < Metacrun
 
     source.datafields('PLK').each do |datafield|
            typ        = datafield.subfields('a').value
-           url        = datafield.subfields('b').value
+           sys        = datafield.subfields('b').value
            label      = datafield.subfields('n').value
         #fulltext_links << url       
         #fulltext_links << label
          
     relationlink << {
             typ: typ,
-            sys_no: url,
+            sys_no: sys,
             label: label
          } 
       end
     
     
-    relationlink.compact.presence.map(&:to_json)
+    relationlink.flatten.select { |relationlink| relationlink[:label].present? }.map(&:to_json)
   end
 end
 
