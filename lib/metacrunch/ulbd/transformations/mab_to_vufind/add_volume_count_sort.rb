3f9b5ad5345559e22df702419efec4905174bd64
Heinz-Peter Berg <Heinz-Peter.Berg@ulb.hhu.de>
2016-10-14 13:44:58 +0200

    volume_count_sort_int in volume_count_sort_str umbenannt

--- lib/metacrunch/ulbd/transformations/mab_to_vufind/add_volume_count_sort.rb
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_volume_count_sort.rb
@@ -4,7 +4,7 @@ require_relative "../mab_to_vufind"
 
 class Metacrunch::ULBD::Transformations::MabToVufind::AddVolumeCountSort < Metacrunch::Transformator::Transformation::Step
   def call
-    target ? Metacrunch::Hash.add(target, "volume_count_sort_int", volume_count_sort) : volume_count_sort
+    target ? Metacrunch::Hash.add(target, "volume_count_sort_str", volume_count_sort) : volume_count_sort
   end
 
   private
@@ -14,6 +14,6 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddVolumeCountSort < Metac
     possible_values << source.datafields('090', ind2: '1').subfields('a').value
     (451..491).step(10) { |f| possible_values << source.datafields("#{f+5}", ind2: '1').subfields('a').value }
     count = possible_values.map(&:presence).compact.uniq.first
-    #count.rjust(15, '0') if count.present?
+    count.rjust(9, '0') if count.present?
   end
 end
