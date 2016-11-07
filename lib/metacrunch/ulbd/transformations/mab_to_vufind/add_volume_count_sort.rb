1c2c51f5028137f89998e83c549e67e4fd596027
Jörg Siepmann <Joerg.Siepmann@ulb.hhu.de>
2016-11-07 16:25:13 +0100

    Felder vertauscht wg. besser Sortierung von komplexen Zählungen

--- lib/metacrunch/ulbd/transformations/mab_to_vufind/add_volume_count_sort.rb
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_volume_count_sort.rb
@@ -14,6 +14,24 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddVolumeCountSort < Metac
     possible_values << source.datafields('090', ind2: '1').subfields('a').value
     (451..491).step(10) { |f| possible_values << source.datafields("#{f+5}", ind2: '1').subfields('a').value }
     count = possible_values.map(&:presence).compact.uniq.first
-    count.rjust(9, '0') if count.present?
+
+    if count.present?
+      # extract the count groups
+      count_groups = count.split(',')
+      # make sure we have always 4 groups
+      count_groups = Array.new(4) { |i| count_groups[i] }
+      # make sure every group is a string
+      count_groups.map!{ |g| g.to_s }
+      # clean things up a bit
+      count_groups.map!{ |g| g.gsub(/\[|\]/, '').strip }
+      # fill up each group to 6 characters
+      count_groups.map!{ |g| g.rjust(6, '0') }
+      # make sure each group is really 6 characters.
+      count_groups.map!{ |g| g[0..5] }
+      # finally join
+      count_groups.join
+    else
+      nil
+    end
   end
 end
--- lib/metacrunch/ulbd/transformations/mab_to_vufind/add_volume_count_sort2.rb
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_volume_count_sort2.rb
@@ -4,34 +4,16 @@ require_relative "../mab_to_vufind"
 
 class Metacrunch::ULBD::Transformations::MabToVufind::AddVolumeCountSort2 < Metacrunch::Transformator::Transformation::Step
   def call
-    target ? Metacrunch::Hash.add(target, "volume_count_sort2_str", volume_count_sort2) : volume_count_sort2
+    target ? Metacrunch::Hash.add(target, "volume_count_sort2_str", volume_count_sort) : volume_count_sort
   end
 
   private
 
-  def volume_count_sort2
+  def volume_count_sort
     possible_values = []
     possible_values << source.datafields('090', ind2: '1').subfields('a').value
     (451..491).step(10) { |f| possible_values << source.datafields("#{f+5}", ind2: '1').subfields('a').value }
     count = possible_values.map(&:presence).compact.uniq.first
-
-    if count.present?
-      # extract the count groups
-      count_groups = count.split(',')
-      # make sure we have always 4 groups
-      count_groups = Array.new(4) { |i| count_groups[i] }
-      # make sure every group is a string
-      count_groups.map!{ |g| g.to_s }
-      # clean things up a bit
-      count_groups.map!{ |g| g.gsub(/\[|\]/, '').strip }
-      # fill up each group to 6 characters
-      count_groups.map!{ |g| g.rjust(6, '0') }
-      # make sure each group is really 6 characters.
-      count_groups.map!{ |g| g[0..5] }
-      # finally join
-      count_groups.join
-    else
-      nil
-    end
+    count.rjust(9, '0') if count.present?
   end
 end
