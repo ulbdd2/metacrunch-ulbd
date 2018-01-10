0546d4162cd497c533bec85385dacfed88c15b0f
Jörg Siepmann <Joerg.Siepmann@ulb.hhu.de>
2018-01-10 14:52:17 +0100

    ADAM

--- lib/metacrunch/ulbd/transformations/mab_to_vufind/add_link_to_toc.rb
+++ lib/metacrunch/ulbd/transformations/mab_to_vufind/add_link_to_toc.rb
@@ -13,18 +13,25 @@ class Metacrunch::ULBD::Transformations::MabToVufind::AddLinkToToc < Metacrunch:
     toc_links = []
 
     source.datafields('655').each do |datafield|
-      url        = datafield.subfields('u').value
-      subfield_3 = datafield.subfields('3') # HBZ Inhaltsverzeichnisse
+      url        = datafield.subfields(['u','y']).values
+      subfield_u = datafield.subfields('u').value
+      subfield_y = datafield.subfields('y') # HBZ Inhaltsverzeichnisse
       subfield_z = datafield.subfields('z') # BVB Inhaltsverzeichnisse
       subfield_t = datafield.subfields('t') # Type: VIEW => Adam Inhaltsverzeichnis
 
-      if (url.present? && subfield_3.present? && subfield_3.value =~ /^inhaltsv/i) ||
-         (url.present? && subfield_z.present? && subfield_z.value =~ /^inhaltsv/i) ||
-         (url.present? && subfield_t.present? && subfield_t.value =~ /^view/i)
+      if (url.present? && subfield_y.present? && subfield_y.value =~ /^inhaltsv/i) ||
+          (url.present? && subfield_y.present? && subfield_y.value =~ /^vorwort/i) ||
+          (url.present? && subfield_y.present? && subfield_y.value =~ /^kapitel/i) ||
+          (url.present? && subfield_y.present? && subfield_y.value =~ /^buchumschlag/i) ||
+         (url.present? && subfield_z.present? && subfield_z.value =~ /^inhaltsv/i) 
         toc_links << url
+        elsif
+        (subfield_u.present? && subfield_t.present? && subfield_t.value =~ /^view/i)
+        toc_links << subfield_u
       end
     end
 
     toc_links.compact.presence
-  end
+    print toc_links
+    end
 end
